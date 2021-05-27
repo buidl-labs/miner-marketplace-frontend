f36c7d1448f75a4cf4c8e53565ea8689ad95c488
Aayush Saini <aayushsaini.77@gmail.com>
2021-05-27 23:42:30 +0530

    completed HomePage

--- pages/index.js
+++ pages/index.js
@@ -16,17 +16,19 @@ import Features from "./components/landingPage/Features";
 import FilecoinStats from "./components/landingPage/FilecoinStats";
 import JoinNetwork from "./components/landingPage/JoinNetwork";
 import Footer from "./components/landingPage/Footer";
+import Faq from "./components/Faq";
 
 const IndexPage = () => {
   return (
     <>
       <Navbar />
-      <Container maxW="container.xl">
+      <Container maxW="container.xl" mb="16">
         <Stack spacing="28">
           <Hero />
           <Features />
           <FilecoinStats />
           <JoinNetwork />
+          <Faq />
         </Stack>
       </Container>
       <Footer />
