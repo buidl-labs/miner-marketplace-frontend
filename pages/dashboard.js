d2b0f6c773c4edb84a77d4c2e9e74e4a4d1f4c1b
Aayush Saini <aayushsaini.77@gmail.com>
2021-05-31 19:09:08 +0530

    added quote calculator

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -13,6 +13,8 @@ import React from "react";
 import DashboardMenu from "../components/dashboard/DashboardMenu";
 import DashboardNavbar from "../components/dashboard/DashboardNavbar";
 import PersonalDetails from "../components/dashboard/PersonalDetails";
+import QuoteCalculator from "../components/dashboard/QuoteCalculator";
+import Scores from "../components/dashboard/Scores";
 
 const Dashboard = () => {
   return (
@@ -20,11 +22,12 @@ const Dashboard = () => {
       <DashboardNavbar />
       <Grid
         h="200px"
-        templateRows="repeat(2, 1fr)"
+        templateRows="repeat(4, 1fr)"
         templateColumns="repeat(5, 1fr)"
         gap="0.5"
+        pr="12"
       >
-        <GridItem rowSpan="2" colSpan="1">
+        <GridItem rowSpan="4" colSpan="1">
           <DashboardMenu />
         </GridItem>
         <GridItem colSpan="2" pt="8">
@@ -39,6 +42,12 @@ const Dashboard = () => {
             ownerAddress="1234556"
             minerBio="this is miner's bio"
           />
+        </GridItem>
+        <GridItem colSpan="2" pt="8">
+          <Scores reputationScore="64" transparencyScore="92" />
+        </GridItem>
+        <GridItem colSpan="1">
+          <QuoteCalculator />
         </GridItem>
       </Grid>
     </>
