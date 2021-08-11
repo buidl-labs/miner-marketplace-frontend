0a8da61cb96562bf9900eea2fe12798bafcc9318
Aayush Saini <aayushsaini.77@gmail.com>
2021-08-11 21:08:44 +0530

    updated: about page content

--- pages/about.js
+++ pages/about.js
@@ -33,7 +33,7 @@ const about = () => {
         <title>About Us - Team DataStation</title>
       </Head>
       <Navbar />
-      <Container maxW="container.xl" mb="40" pt="28">
+      <Container maxW={{ lg: "container.xl" }} mb="16" pt="20">
         <Stack textAlign="center" py="12">
           <Heading size="2xl" color="gray.900" pb="2">
             We are BUIDL-ers!
@@ -59,34 +59,38 @@ const about = () => {
             gap={16}
           >
             <GridItem colSpan="1">
-              <HStack alignItems="flex-start" spacing="6" w="32rem">
-                <Image src="/images/team/saumay.png" maxW="28" alt="dp" />
+              <HStack alignItems="flex-start" spacing="6" maxW="32rem">
+                <Image
+                  src="/images/team/saumay.png"
+                  maxW={{ md: "28", base: "16" }}
+                  alt="dp"
+                />
                 <VStack alignItems="flex-start">
                   <Heading size="md" color="gray.800">
                     Saumay Agrawal
                   </Heading>
-                  <Text size="4" color="color.600" fontWeight="medium">
+                  <Text size="4" color="blue.700" fontWeight="bold">
                     Project Lead
                   </Text>
                   <Text pb="2" textAlign="left">
                     A technopreneur thriving at the bleeding edge of cyberspace.
                     Building in Web3/blockchain space at present.
                   </Text>
-                  <HStack spacing="6">
+                  <HStack spacing="4">
                     <Link href="https://twitter.com/saumay_agrawal" isExternal>
-                      <Icon as={FaTwitter} h={5} w={5} color="gray.600" />
+                      <Icon as={FaTwitter} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link
                       href="https://www.linkedin.com/in/saumayagrawal/"
                       isExternal
                     >
-                      <Icon as={FaLinkedinIn} h={5} w={5} color="gray.600" />
+                      <Icon as={FaLinkedinIn} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link href="https://github.com/Saumay-Agrawal" isExternal>
-                      <Icon as={FaGithub} h={5} w={5} color="gray.600" />
+                      <Icon as={FaGithub} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link href="https://saumay.com" isExternal>
-                      <Icon as={FaGlobe} h={5} w={5} color="gray.600" />
+                      <Icon as={FaGlobe} h={5} w={5} color="gray.500" />
                     </Link>
                   </HStack>
                 </VStack>
@@ -94,34 +98,38 @@ const about = () => {
             </GridItem>
 
             <GridItem colSpan="1">
-              <HStack alignItems="flex-start" spacing="6" w="32rem">
-                <Image src="/images/team/rajdeep.png" maxW="28" alt="dp" />
+              <HStack alignItems="flex-start" spacing="6" maxW="32rem">
+                <Image
+                  src="/images/team/rajdeep.png"
+                  maxW={{ md: "28", base: "16" }}
+                  alt="dp"
+                />
                 <VStack alignItems="flex-start">
                   <Heading size="md" color="gray.800">
                     Rajdeep Bharati
                   </Heading>
-                  <Text size="4" color="color.600" fontWeight="medium">
+                  <Text size="4" color="blue.700" fontWeight="bold">
                     Research & Engineering Lead
                   </Text>
                   <Text pb="2" textAlign="left">
                     Studying crypto-economic systems that help people play
                     positive sum games.
                   </Text>
-                  <HStack spacing="6">
+                  <HStack spacing="4">
                     <Link href="https://twitter.com/ImRajdeepB" isExternal>
-                      <Icon as={FaTwitter} h={5} w={5} color="gray.600" />
+                      <Icon as={FaTwitter} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link
                       href="https://www.linkedin.com/in/rajdeepbharati/"
                       isExternal
                     >
-                      <Icon as={FaLinkedinIn} h={5} w={5} color="gray.600" />
+                      <Icon as={FaLinkedinIn} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link href="https://github.com/rajdeepbh" isExternal>
-                      <Icon as={FaGithub} h={5} w={5} color="gray.600" />
+                      <Icon as={FaGithub} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link href="https://rajdeepbharati.com/" isExternal>
-                      <Icon as={FaGlobe} h={5} w={5} color="gray.600" />
+                      <Icon as={FaGlobe} h={5} w={5} color="gray.500" />
                     </Link>
                   </HStack>
                 </VStack>
@@ -129,30 +137,73 @@ const about = () => {
             </GridItem>
 
             <GridItem colSpan="1">
-              <HStack alignItems="flex-start" spacing="6" w="32rem">
-                <Image src="/images/team/aayush.png" maxW="28" alt="dp" />
+              <HStack alignItems="flex-start" spacing="6" maxW="32rem">
+                <Image
+                  src="/images/team/prastut.png"
+                  maxW={{ md: "28", base: "16" }}
+                  alt="dp"
+                />
+                <VStack alignItems="flex-start">
+                  <Heading size="md" color="gray.800">
+                    Prastut Kumar
+                  </Heading>
+                  <Text size="4" color="blue.700" fontWeight="bold">
+                    Advisor
+                  </Text>
+                  <Text pb="2" textAlign="left">
+                    Insatiably curious. Currently a student of cryptoeconomics
+                    and reality.
+                  </Text>
+                  <HStack spacing="4">
+                    <Link href="https://twitter.com/prastutkumar" isExternal>
+                      <Icon as={FaTwitter} h={5} w={5} color="gray.500" />
+                    </Link>
+                    <Link
+                      href="https://www.linkedin.com/in/prastut/"
+                      isExternal
+                    >
+                      <Icon as={FaLinkedinIn} h={5} w={5} color="gray.500" />
+                    </Link>
+                    <Link href="https://github.com/prastut" isExternal>
+                      <Icon as={FaGithub} h={5} w={5} color="gray.500" />
+                    </Link>
+                    <Link href="https://prastutkumar.com/" isExternal>
+                      <Icon as={FaGlobe} h={5} w={5} color="gray.500" />
+                    </Link>
+                  </HStack>
+                </VStack>
+              </HStack>
+            </GridItem>
+
+            <GridItem colSpan="1">
+              <HStack alignItems="flex-start" spacing="6" maxW="32rem">
+                <Image
+                  src="/images/team/aayush.png"
+                  maxW={{ md: "28", base: "16" }}
+                  alt="dp"
+                />
                 <VStack alignItems="flex-start">
                   <Heading size="md" color="gray.800">
                     Aayush Saini
                   </Heading>
-                  <Text size="4" color="color.600" fontWeight="medium">
+                  <Text size="4" color="blue.700" fontWeight="bold">
                     Design Technologist
                   </Text>
                   <Text pb="2" textAlign="left">
                     Product Designer during day, 3D Artist by Night!
                   </Text>
-                  <HStack spacing="6">
+                  <HStack spacing="4">
                     <Link href="https://twitter.com/aayushsaini_" isExternal>
-                      <Icon as={FaTwitter} h={5} w={5} color="gray.600" />
+                      <Icon as={FaTwitter} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link href="https://linkedin.com/in/aayushsaini" isExternal>
-                      <Icon as={FaLinkedinIn} h={5} w={5} color="gray.600" />
+                      <Icon as={FaLinkedinIn} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link href="https://github.com/aayushsaini1" isExternal>
-                      <Icon as={FaGithub} h={5} w={5} color="gray.600" />
+                      <Icon as={FaGithub} h={5} w={5} color="gray.500" />
                     </Link>
                     <Link href="https://aayushsaini.me" isExternal>
-                      <Icon as={FaGlobe} h={5} w={5} color="gray.600" />
+                      <Icon as={FaGlobe} h={5} w={5} color="gray.500" />
                     </Link>
                   </HStack>
                 </VStack>
