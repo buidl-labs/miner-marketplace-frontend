48d92568e411e1f72216664e41f516b1007bc6d3
Aayush Saini <aayushsaini.77@gmail.com>
2021-08-11 16:39:17 +0530

    added legal pages content

--- pages/disclaimer.js
+++ pages/disclaimer.js
@@ -7,6 +7,12 @@ import {
   Button,
   Text,
   VStack,
+  Link,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
 } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../components/Navbar";
@@ -20,9 +26,7 @@ const disclaimer = () => {
   return (
     <>
       <Head>
-        <title>
-          Miner Marketplace - Disclaimer
-        </title>
+        <title>Legal Disclaimer - DataStation</title>
       </Head>
       <Navbar />
       <Container
@@ -33,7 +37,146 @@ const disclaimer = () => {
       >
         <Stack textAlign="center" alignItems="center" py="32">
           <Heading>Legal Disclaimer</Heading>
-          <Text>for Miner marketplace</Text>
+
+          <Stack textAlign="left" spacing="2" pt="4" fontSize="md">
+            <Heading size="md" pt="4">
+              1. Information published on datastation.app
+            </Heading>
+            <Text>
+              The website{" "}
+              <Link href="https://datastation.app">
+                https://datastation.app/
+              </Link>{" "}
+              (hereinafter, referred to as the "Website") provides information
+              and material of a general nature. You are not authorized and nor
+              should you rely on the Website for legal advice, business advice,
+              or advice of any kind. You act at your own risk in reliance on the
+              contents of the Website. Should you make a decision to act or not
+              act you should contact a licensed attorney in the relevant
+              jurisdiction in which you want or need help. In no way are the
+              owners of, or contributors to, the Website responsible for the
+              actions, decisions, or other behavior taken or not taken by you in
+              reliance upon the Website.
+            </Text>
+            <Heading size="md" pt="4">
+              2. Translations
+            </Heading>
+            <Text>
+              The Website may contain translations of the English version of the
+              content available on the Website. These translations are provided
+              only as a convenience. In the event of any conflict between the
+              English language version and the translated version, the English
+              language version shall take precedence. If you notice any
+              inconsistency, please report them on GitHub.
+            </Text>
+            <Heading size="md" pt="4">
+              3. Risks related to the use of the Website
+            </Heading>
+            <Text>
+              The Website will not be responsible for any losses, damages or
+              claims arising from events falling within the scope of the
+              following five categories:
+              <OrderedList mt="4" spacing="2">
+                <ListItem>
+                  Mistakes made by the user of any kind, including but not
+                  limited to, forgotten passwords, payments sent to wrong
+                  addresses, and accidental deletion of wallets.
+                </ListItem>
+                <ListItem>
+                  Software problems of the Website and/or any investment-related
+                  software or service, e.g., corrupted wallet file, incorrectly
+                  constructed transactions, unsafe cryptographic libraries,
+                  malware affecting the Website and/or any Bitcoin-related
+                  software or service.
+                </ListItem>
+                <ListItem>
+                  Technical failures in the hardware of the user, e.g., data
+                  loss due to a faulty or damaged storage device.
+                </ListItem>
+                <ListItem>
+                  Security problems experienced by the user, e.g., unauthorized
+                  access to users' wallets and/or accounts.
+                </ListItem>
+                <ListItem>
+                  Actions or inactions of third parties and/or events
+                  experienced by third parties, e.g., bankruptcy of service
+                  providers, information security attacks on service providers,
+                  and fraud conducted by third parties.
+                </ListItem>
+              </OrderedList>
+            </Text>
+            <Heading size="md" pt="4">
+              4. Investment risks
+            </Heading>
+            <Text>
+              The investment in cryptocurrencies can lead to loss of money over
+              short or even long periods. The investors in cryptocurrencies
+              should expect prices to have large range fluctuations. The
+              information published on the Website cannot guarantee that the
+              investors in cryptocurrencies would not lose money.
+            </Text>
+            <Text></Text>
+            <Heading color="gray.600" size="sm" pt="2">
+              Protocol specific risks
+            </Heading>
+            <Text>
+              Each decentralized network has it's own protocol, where each
+              protocol might be different from the rest. By participating in
+              these protocols you might expose yourself to several protocol
+              specific risks, including but not limited to, slashing/loss of
+              funds, burning of unclaimed rewards, locking of tokens for some
+              time period or failure of the protocol itself. You act at your own
+              risk when participating in any such protocol. In no way are the
+              owners of, or contributors to, the Website responsible for
+              mitigating such risks.
+            </Text>
+            <Heading size="md" pt="4">
+              5. Compliance with tax obligations
+            </Heading>
+            <Text>
+              The users of the Website are solely responsible to determinate
+              what, if any, taxes apply to their transactions. The owners of, or
+              contributors to, the Website are NOT responsible for determining
+              the taxes that apply to transactions.
+            </Text>
+            <Heading size="md" pt="4">
+              6. The Website does not store, send, or receive cryptocurrencies
+            </Heading>
+            <Text>
+              The Website does not store, send or receive cryptocurrencies. This
+              is because cryptocurrencies exist only by virtue of the ownership
+              record maintained in the respective cryptocurrency network. Any
+              transfer of title in cryptocurrencies occurs within their
+              respective decentralized networks, and not on the Website.
+            </Text>
+            <Heading size="md" pt="4">
+              7. No warranties
+            </Heading>
+            <Text>
+              The Website is provided on an "as is" basis without any warranties
+              of any kind regarding the Website and/or any content, data,
+              materials and/or services provided on the Website.
+            </Text>
+            <Heading size="md" pt="4">
+              8. Limitation of liability
+            </Heading>
+            <Text>
+              Unless otherwise required by law, in no event shall the owners of,
+              or contributors to, the Website be liable for any damages of any
+              kind, including, but not limited to, loss of use, loss of profits,
+              or loss of data arising out of or in any way connected with the
+              use of the Website.
+            </Text>
+            <Heading size="md" pt="4">
+              9. Arbitration
+            </Heading>
+            <Text>
+              The user of the Website agrees to arbitrate any dispute arising
+              from or in connection with the Website or this disclaimer, except
+              for disputes related to copyrights, logos, trademarks, trade
+              names, trade secrets or patents.
+            </Text>
+          </Stack>
         </Stack>
       </Container>
 
--- pages/privacy.js
+++ pages/privacy.js
@@ -7,12 +7,18 @@ import {
   Button,
   Text,
   VStack,
+  Link,
+  List,
+  ListItem,
+  ListIcon,
+  OrderedList,
+  UnorderedList,
 } from "@chakra-ui/react";
 import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/landingPage/Footer";
 import { useRouter } from "next/router";
-import Head from "next/head"
+import Head from "next/head";
 
 const privacy = () => {
   const router = useRouter();
@@ -20,9 +26,7 @@ const privacy = () => {
   return (
     <>
       <Head>
-        <title>
-          Miner Marketplace - Privacy Policy
-        </title>
+        <title>Privacy Policy - DataStation</title>
       </Head>
       <Navbar />
       <Container
@@ -33,7 +37,198 @@ const privacy = () => {
       >
         <Stack textAlign="center" alignItems="center" py="32">
           <Heading>Privacy Policy</Heading>
-          <Text>for Miner marketplace</Text>
+
+          <Stack textAlign="left" spacing="2" fontSize="md" pt="6">
+            <Text>
+              At DataStation, accessible from{" "}
+              <Link href="https://dataStation.app">
+                https://datastation.app
+              </Link>{" "}
+              one of our main priorities is the privacy of our visitors. This
+              Privacy Policy document contains types of information that is
+              collected and recorded by DataStation and how we use it.
+            </Text>
+            <Text>
+              If you have additional questions or require more information about
+              our Privacy Policy, do not hesitate to contact us.
+            </Text>
+            <Text>
+              This Privacy Policy applies only to our online activities and is
+              valid for visitors to our website with regards to the information
+              that they shared and/or collect in DataStation. This policy is not
+              applicable to any information collected offline or via channels
+              other than this website.
+            </Text>
+            <Heading size="md" pt="4">
+              Consent
+            </Heading>
+            <Text>
+              By using our website, you hereby consent to our Privacy Policy and
+              agree to its terms.
+            </Text>
+            <Heading size="md" pt="4">
+              Information we collect
+            </Heading>
+            <Text>
+              The personal information that you are asked to provide, and the
+              reasons why you are asked to provide it, will be made clear to you
+              at the point we ask you to provide your personal information.
+            </Text>
+            <Text>
+              If you contact us directly, we may receive additional information
+              about you such as your name, email address, phone number, the
+              contents of the message and/or attachments you may send us, and
+              any other information you may choose to provide.
+            </Text>
+            <Heading size="md" pt="4">
+              How we use your information
+            </Heading>
+            <Text>
+              We use the information we collect in various ways, including to:
+              <UnorderedList mt="4" spacing="2">
+                <ListItem>Provide, operate, and maintain our website</ListItem>
+                <ListItem>
+                  Improve, personalize, and expand our website
+                </ListItem>
+                <ListItem>
+                  Understand and analyze how you use our website
+                </ListItem>
+                <ListItem>
+                  Develop new products, services, features, and functionality
+                </ListItem>
+                <ListItem>
+                  Communicate with you, either directly or through one of our
+                  partners, including for customer service, to provide you with
+                  updates and other information relating to the website, and for
+                  marketing and promotional purposes if you opt in for the same
+                </ListItem>
+                <ListItem>Send you emails</ListItem>
+                <ListItem>Find and prevent fraud</ListItem>
+              </UnorderedList>
+            </Text>
+            <Heading size="md" pt="4">
+              Log Files
+            </Heading>
+            <Text>
+              DataStation follows a standard procedure of using log files. These
+              files log visitors when they visit websites. All hosting companies
+              do this as part of hosting services' analytics. The information
+              collected by log files include internet protocol (IP) addresses,
+              browser type, Internet Service Provider (ISP), date and time
+              stamp, referring/exit pages, and possibly the number of clicks.
+              These are not linked to any information that is personally
+              identifiable. The purpose of the information is for analyzing
+              trends, administering the site, tracking users' movement on the
+              website, and gathering demographic information.
+            </Text>
+            <Heading size="md" pt="4">
+              Cookies and Web Beacons
+            </Heading>
+            <Text>
+              Like any other website, DataStation uses 'cookies'. These cookies
+              are used to store information including visitors' preferences, and
+              the pages on the website that the visitor accessed or visited. The
+              information is used to optimize the users' experience by
+              customizing our web page content based on visitors' browser type
+              and/or other information.
+            </Text>
+            <Text>
+              For more general information on cookies, please read{" "}
+              <Link
+                href="https://www.cookieconsent.com/what-are-cookies/"
+                isExternal
+              >
+                "What Are Cookies".
+              </Link>
+            </Text>
+            <Heading size="md" pt="4">
+              CCPA Privacy Rights
+            </Heading>
+            <Text>
+              Under the CCPA, among other rights, California consumers have the
+              right to:
+              <UnorderedList mt="4" spacing="2">
+                <ListItem>
+                  Request that a business that collects a consumer's personal
+                  data disclose the categories and specific pieces of personal
+                  data that a business has collected about consumers.
+                </ListItem>
+                <ListItem>
+                  Request that a business delete any personal data about the
+                  consumer that a business has collected.
+                </ListItem>
+                <ListItem>
+                  Request that a business that sells a consumer's personal data,
+                  not sell the consumer's personal data.
+                </ListItem>
+              </UnorderedList>
+            </Text>
+            <Text>
+              Please note that we do not sell any personal data. If you make a
+              request, we have one month to respond to you. If you would like to
+              exercise any of these rights, please contact us.
+            </Text>
+            <Heading size="md" pt="4">
+              GDPR Data Protection Rights
+            </Heading>
+            <Text>
+              We would like to make sure you are fully aware of all of your data
+              protection rights. Every user is entitled to the following:
+              <UnorderedList mt="4" spacing="2">
+                <ListItem>
+                  The right to access – You have the right to request copies of
+                  your personal data. We may charge you a small fee for this
+                  service.
+                </ListItem>
+                <ListItem>
+                  The right to rectification – You have the right to request
+                  that we correct any information you believe is inaccurate. You
+                  also have the right to request that we complete the
+                  information you believe is incomplete.
+                </ListItem>
+                <ListItem>
+                  The right to erasure – You have the right to request that we
+                  erase your personal data, under certain conditions.
+                </ListItem>
+                <ListItem>
+                  The right to restrict processing – You have the right to
+                  request that we restrict the processing of your personal data,
+                  under certain conditions.
+                </ListItem>
+                <ListItem>
+                  The right to object to processing – You have the right to
+                  object to our processing of your personal data, under certain
+                  conditions.
+                </ListItem>
+                <ListItem>
+                  The right to data portability – You have the right to request
+                  that we transfer the data that we have collected to another
+                  organization, or directly to you, under certain conditions.
+                </ListItem>
+              </UnorderedList>
+            </Text>
+            <Text>
+              If you make a request, we have one month to respond to you. If you
+              would like to exercise any of these rights, please contact us.
+            </Text>
+            <Heading size="md" pt="4">
+              Children's Information
+            </Heading>
+            <Text>
+              Another part of our priority is adding protection for children
+              while using the internet. We encourage parents and guardians to
+              observe, participate in, and/or monitor and guide their online
+              activity.
+            </Text>
+            <Text>
+              DataStation does not knowingly collect any Personal Identifiable
+              Information from children under the age of 13. If you think that
+              your child provided this kind of information on our website, we
+              strongly encourage you to contact us immediately and we will do
+              our best efforts to promptly remove such information from our
+              records.
+            </Text>
+          </Stack>
         </Stack>
       </Container>
 
