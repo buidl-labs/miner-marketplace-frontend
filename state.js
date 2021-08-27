6b694bed7bb67cfd4a04f25ab921be6c31e2a4cd
Rajdeep Bharati <rajdeepbh@protonmail.com>
2021-08-27 20:54:00 +0530

    create Dockerfile

--- state.js
+++ state.js
@@ -4,6 +4,12 @@ const initState = {
   ledgerAddr: "",
   isSignedIn: false,
   authMode: "",
+  activeMinersCount: 3500,
+  networkStorageCapacity: "11.259 EB",
+  dataStored: "2.977 PB",
+  topMinerBlockRewards24H: "4797430961442729373635",
+  totalBlockRewards24H: "345904011472021195844911",
+  averageDealPrice: "5881975665920003",
 };
 // authMode: "lotus" or "ledger"
 export const { useGlobalState } = createGlobalState(initState);
